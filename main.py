fd332350e076dfd908d2e726f309cd452ef4a0a4
Alex <kibgheeo775533@gmail.com>
2025-11-19 21:46:18 +0500

    starting the authentication

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
 from infrastructure.database import Base, engine
+from infrastructure.database import get_db
+from sqlalchemy.orm import Session
+from domain.models import User
  
-app = FastAPI()
+app = FastAPI(
+    title="ChatApp"
+)
  
+@app.get("/")
+async def getUsers(db : Session = Depends(get_db)):
+    users = db.query(User).all()
+    if len(users) == 0:
+        raise HTTPException(
+            status_code=404,
+            detail="No users yet! Soon"
+        )
+        
