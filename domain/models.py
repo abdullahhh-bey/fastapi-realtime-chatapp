e0cca468540404dd9fb73efdf012d13bfc764ca0
Alex <kibgheeo775533@gmail.com>
2025-11-23 02:29:14 +0500

    Implementing real time chat wiht web socket

--- domain/models.py
+++ domain/models.py
@@ -12,3 +12,14 @@ class User(Base):
     isRegistered = Column(Boolean , default = False)
     createdAt = Column(DateTime , default = datetime.utcnow)
 
+    
+
+class Message(Base):
+    __tablename__ = "messages"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    sender_id = Column(Integer, ForeignKey("users.id"))  
+    receiver_id = Column(Integer, ForeignKey("users.id")) 
+    content = Column(String, nullable=False)  
+    created_at = Column(DateTime, server_default=func.now()) 
+
