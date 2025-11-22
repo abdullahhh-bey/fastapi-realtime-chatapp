3a5798accf2364e6ae21d4a0d6a2c6c03e18c35c
Alex <kibgheeo775533@gmail.com>
2025-11-23 02:46:06 +0500

    Added the model table and made the migration

--- domain/models.py
+++ domain/models.py
@@ -1,25 +1,51 @@
 from infrastructure.database import Base
-from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey, func
+from sqlalchemy.orm import relationship
 from datetime import datetime
+
 
 class User(Base):
     __tablename__ = "users"
     
     id = Column(Integer, nullable=False, primary_key=True)
-    username = Column(String(150) , nullable=False, index=True)    
+    username = Column(String(150), nullable=False, index=True)    
     email = Column(String, nullable=False)
     hashed_password = Column(String)
-    isRegistered = Column(Boolean , default = False)
-    createdAt = Column(DateTime , default = datetime.utcnow)
+    isRegistered = Column(Boolean, default=False)
+    createdAt = Column(DateTime, default=datetime.utcnow)
 
+    sent_messages = relationship(
+        "Message",
+        foreign_keys="Message.sender_id",
+        back_populates="sender",
+        cascade="all, delete-orphan"
+    )
     
+    received_messages = relationship(
+        "Message",
+        foreign_keys="Message.receiver_id",
+        back_populates="receiver",
+        cascade="all, delete-orphan"
+    )
+
 
 class Message(Base):
     __tablename__ = "messages"
     
     id = Column(Integer, primary_key=True, index=True)
-    sender_id = Column(Integer, ForeignKey("users.id"))  
-    receiver_id = Column(Integer, ForeignKey("users.id")) 
-    content = Column(String, nullable=False)  
-    created_at = Column(DateTime, server_default=func.now()) 
+    sender_id = Column(Integer, ForeignKey("users.id"))
+    receiver_id = Column(Integer, ForeignKey("users.id"))
+    content = Column(String, nullable=False)
+    created_at = Column(DateTime, server_default=func.now())
 
+    sender = relationship(
+        "User",
+        foreign_keys=[sender_id],
+        back_populates="sent_messages"
+    )
+
+    receiver = relationship(
+        "User",
+        foreign_keys=[receiver_id],
+        back_populates="received_messages"
+    )
